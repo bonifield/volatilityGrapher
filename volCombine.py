4cb71643da717b3a8aca98873d5144d828c9f3ce
bonifield <bonifield@users.noreply.github.com>
2017-05-27 17:23:22 -0500

    Update volCombine.py

--- volCombine.py
+++ volCombine.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 
 #==============
-# volCombine.py v1.0
+# volCombine.py v1.1
 # 27 May 2017
 # Feed the script plaintext output from envars and pslist (Volatility modules)
-# Usage:  volCombine.py envars.txt pslist.txt
-# TODO:  add psscan times to node label
+# Usage:  volCombine.py envars.txt pslist.txt psscan.txt
+# TODO:  add style="filled", fillcolor="orange" for future node-specific enhancements
 #==============
 
 import os,re,sys,time
@@ -13,20 +13,27 @@ import os,re,sys,time
 syslist = sys.argv
 psli = ''
 enva = ''
+pssc = ''
 epoch = int(time.time())
 
 #==============
 
-if len(syslist) == 3:
+if len(syslist) > 1:
 	for i in syslist:
 		if re.search('pslist', i):
 			psli = i
 		if re.search('envar', i):
 			enva = i
+		if re.search('psscan', i):
+			pssc = i
+
 else:
 	print
-	print 'You need at least the following:  envars.txt and pslist.txt'
-	print 'Usage:  volCombine.py envars.txt pslist.txt'
+	print 'You need at least pslist.txt and one or more of the following:  envars.txt,  psscan.txt'
+	print 'Usage examples:'
+	print 'volCombine.py pslist.txt'
+	print 'volCombine.py pslist.txt envars.txt'
+	print 'volCombine.py pslist.txt envars.txt psscan.txt'
 	print
 	sys.exit(1)
 
@@ -43,56 +50,82 @@ def makeGraph():
 	print 'Making combined graph.  This may take a second...'
 	listy = []
 	dicty = {}
-	u = re.compile('USERNAME')
-	with open(psli,'r') as f:
-		k = ''
-		v = ''
-		for i,line in enumerate(f):
-			if i >= 2:
-				l = line.split()
-				name = l[1]
-				pid = l[2]
-				ppid = l[3]
-				listy.append('"%s" -> "%s" [color="black"];' % (ppid, pid))
-				k = pid
-				v = name
-			if k not in dicty.keys():
-				dicty.setdefault(k,[])
-				dicty[k].append(v)
-			elif k in dicty.keys():
-				dicty[k].append(v)
-	f.close()
 
-	with open(enva, 'r') as f:
-		pid = ''
-		username = ''
-		for i,line in enumerate(f):
-			if i >= 2:
-				if u.findall(line):
+	if psli:
+		with open(psli,'r') as f:
+			k = ''
+			v = ''
+			for i,line in enumerate(f):
+				if i >= 2:
 					l = line.split()
-					pid = l[0]
-					username = l[4]
+					name = l[1]
+					pid = l[2]
+					ppid = l[3]
+					listy.append('"%s" -> "%s" [color="black"];' % (ppid, pid))
+					k = pid
+					v = name
+				if k not in dicty.keys():
+					dicty.setdefault(k,[])
+					dicty[k].append(v)
+				elif k in dicty.keys():
+					dicty[k].append(v)
+		f.close()
+	else:
+		print
+		print 'You need pslist.txt at a minumum to run this script!'
+		print
+		sys.exit(1)
+
+	if enva:
+		with open(enva, 'r') as f:
+			u = re.compile('USERNAME')
+			pid = ''
+			username = ''
+			for i,line in enumerate(f):
+				if i >= 2:
+					if u.findall(line):
+						l = line.split()
+						pid = l[0]
+						username = l[4]
+						if pid not in dicty.keys():
+							dicty.setdefault(pid,[])
+							dicty[pid].append(username)
+						elif pid in dicty.keys():
+							dicty[pid].append(username)
+		f.close()
+
+	if pssc:
+		with open(pssc, 'r') as f:
+			pid = ''
+			t = ''
+			for i, line in enumerate(f):
+				if i >= 2:
+					l = line.split()
+					pid = l[2]
+					t = str(l[5]+' '+l[6]+' '+l[7][:3])
 					if pid not in dicty.keys():
 						dicty.setdefault(pid,[])
-						dicty[pid].append(username)
+						dicty[pid].append(t)
 					elif pid in dicty.keys():
-						dicty[pid].append(username)
+						dicty[pid].append(t)
+		f.close()
 
 	for k in dicty.keys():
 		if len(k) == 0:
 			del dicty[k]
 
 	with open(dotFile,'w') as d:
-		d.write('digraph output {\nnode[shape = Mrecord];\nfontsize=16;\nnodesep=0.5;\nrankdir=LR;')
+		d.write('digraph output {\nnode[shape = Mrecord];\nfontsize=16;\nnodesep=1.5;\nranksep=1;\nrankdir=LR;')
 		for i in list(set(listy)):
 			d.write(i)
-
 		if dicty:
 			for k in dicty.keys():
 				if len(dicty[k]) == 1:
 					d.write('"%s" [label="%s\n%s"]' % (k, k, dicty[k][0]))
 				elif len(dicty[k]) == 2:
 					d.write('"%s" [label="%s\n%s|%s"]' % (k, k, dicty[k][0], dicty[k][1]))
+				elif len(dicty[k]) == 3:
+					d.write('"%s" [label="%s\n%s|%s|%s"]' % (k, k, dicty[k][0], dicty[k][1], dicty[k][2]))
 		d.write('\n}')
 	d.close()
 

546147e6e0d78f8deef291bdea4870891b06fecb
bonifield <bonifield@users.noreply.github.com>
2018-09-09 00:19:38 -0500

    Create ez-volCombine.sh

---
+++ ez-volCombine.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo
+echo "Usage:  ezvol.sh somefile.dmp profile"
+echo "Running.  This may take a minute..."
+vol.py -f $1 --profile=$2 --output=json pslist --output-file=$1-$2-pslist.json 2> /dev/null && \
+vol.py -f $1 --profile=$2 --output=json psscan --output-file=$1-$2-psscan.json 2> /dev/null && \
+vol.py -f $1 --profile=$2 --output=json envars --output-file=$1-$2-envars.json 2> /dev/null && \
+vol.py -f $1 --profile=$2 --output=json malfind --output-file=$1-$2-malfind.json 2> /dev/null && \
+vol.py -f $1 --profile=$2 --output=json netscan --output-file=$1-$2-netscan.json 2> /dev/null && \
+volCombine.py $1-$2-pslist.json $1-$2-psscan.json $1-$2-envars.json $1-$2-malfind.json $1-$2-netscan.json
+echo "...done."
+echo

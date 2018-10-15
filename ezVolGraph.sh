5ef1757dcf3d7f4062a8f43e983d9aaba22956e4
bonifield <bonifield@users.noreply.github.com>
2018-10-15 18:09:51 -0500

    Update ezVolGraph.sh

--- ezVolGraph.sh
+++ ezVolGraph.sh
@@ -8,6 +8,7 @@ vol.py -f $1 --profile=$2 --output=json psscan --output-file=$1-$2-psscan.json 2
 vol.py -f $1 --profile=$2 --output=json envars --output-file=$1-$2-envars.json 2> /dev/null && \
 vol.py -f $1 --profile=$2 --output=json malfind --output-file=$1-$2-malfind.json 2> /dev/null && \
 vol.py -f $1 --profile=$2 --output=json netscan --output-file=$1-$2-netscan.json 2> /dev/null && \
-volGraph.py $1-$2-pslist.json $1-$2-psscan.json $1-$2-envars.json $1-$2-malfind.json $1-$2-netscan.json
+vol.py -f $1 --profile=$2 --output=json cmdline --output-file=$1-$2-cmdline.json 2> /dev/null && \
+volGraph.py $1-$2-pslist.json $1-$2-psscan.json $1-$2-envars.json $1-$2-malfind.json $1-$2-netscan.json $1-$2-cmdline.json
 echo "...done."
 echo
